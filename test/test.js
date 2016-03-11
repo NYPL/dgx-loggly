c4c476e6a53f9ace1f2b8100665b6d7621ad35d8
Sean Redmond <github-smr@sneakemail.com>
2016-03-11 11:53:30 -0500

    Tests for loggly transport config

--- test/test.js
+++ test/test.js
@@ -14,5 +14,28 @@ describe('logger', () => {
       Object.keys(myLogger.transports).should.eql(['console']);
     });
   });
+
+  describe('with an environment', () => {
+    var myLogger;
+
+    before(() => {
+      myLogger = getLogger('TEST', 'production', 'TOKEN', 'SUBDOMAIN');
+    });
+
+    describe('with production', () => {
+      it('only configures a loggly logger', () => {
+	Object.keys(myLogger.transports).length.should.equal(1);
+	Object.keys(myLogger.transports).should.eql(['loggly']);
+      });
+
+      it('has the right token', () => {
+	myLogger.transports.loggly.client.token.should.equal('TOKEN');
+      });
+
+      it('has the right subdomain', () => {
+	myLogger.transports.loggly.client.subdomain.should.equal('SUBDOMAIN');
+      });
+    });
+  });
 });
 
