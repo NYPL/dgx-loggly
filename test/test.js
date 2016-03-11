cf637e5c84d0a6028604e852fbd2e0fff606d444
Sean Redmond <github-smr@sneakemail.com>
2016-03-11 13:41:33 -0500

    Test for non-remote qa config

--- test/test.js
+++ test/test.js
@@ -16,13 +16,12 @@ describe('logger', () => {
   });
 
   describe('with an environment', () => {
-    var myLogger;
-
-    before(() => {
-      myLogger = getLogger('TEST', 'production', 'TOKEN', 'SUBDOMAIN');
-    });
-
     describe('with production', () => {
+      var myLogger;
+      before(() => {
+	myLogger = getLogger('TEST', 'production', 'TOKEN', 'SUBDOMAIN');
+      });
+
       it('only configures a loggly logger', () => {
 	Object.keys(myLogger.transports).length.should.equal(1);
 	Object.keys(myLogger.transports).should.eql(['loggly']);
@@ -36,6 +35,20 @@ describe('logger', () => {
 	myLogger.transports.loggly.client.subdomain.should.equal('SUBDOMAIN');
       });
     });
+
+    describe('with qa', () => {
+      describe('without the remote option', () => {
+	
+	var myLogger;
+	before(() => {
+	  myLogger = getLogger('TEST', 'qa', 'TOKEN', 'SUBDOMAIN');
+	});
+	
+	it('only configures a loggly logger', () => {
+	  Object.keys(myLogger.transports).length.should.equal(1);
+	  Object.keys(myLogger.transports).should.eql(['console']);
+	});
+      });
+    });
   });
 });
-
