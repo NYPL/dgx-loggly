c469c6d01d52d83c57347f394d2ca3c7a093c565
Sean Redmond <github-smr@sneakemail.com>
2016-03-11 16:03:55 -0500

    Add documentation

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -19,6 +19,9 @@ const defaults = {
 
 /**
  * Combine appTag with environment for non-production environments
+ * @param {Object} opts
+ * @return {string[]} Array consisting of the environment-enhanced tag. If 
+ * `env` or `appTag` is not set, an empty array.
  */
 function envTag(opts) {
   const { env, appTag } = opts;
@@ -33,6 +36,13 @@ function envTag(opts) {
   return [];
 }
 
+/**
+ * Merge `opts.appTag` with array of other tags in `opts.tags` if it 
+ * exists. Otherwise return an array of just `opts.appTag` to be passed
+ * to a Loggly transport.
+ * @param {Object} opts
+ * @return {string[]} Array of tags for Loggly use.
+ */
 function mergeTags(opts) {
   const { tags } = opts;
   if (Array.isArray(tags)) {
@@ -42,6 +52,11 @@ function mergeTags(opts) {
   return envTag(opts);
 }
 
+/**
+ * Create a Loggly transport
+ * @param {Object} opts
+ * @return {Object}
+ */ 
 function logglyConfig(opts) {
   return new Winston.transports.Loggly(
     Object.assign(
@@ -52,12 +67,23 @@ function logglyConfig(opts) {
   );
 }
 
+/**
+ * Create a Console transport
+ * @param {Object} opts
+ * @return {Object}
+ */ 
 function consoleConfig(opts) {
   return new Winston.transports.Console(
     Object.assign({}, defaults.console, opts.console)
   );
 }
 
+/**
+ * Configure and return console and Loggly transports based on environment and
+ * other options.
+ * @param {Object} opts
+ * @return {Object[]} Array of transports
+ */
 function getTransports(opts) {
   const { env, remote } = opts;
   if (env) {
@@ -77,6 +103,29 @@ function getTransports(opts) {
   return [consoleConfig(opts)];
 }
 
+
+/**
+ * Get a Winston logger, configured to log to console or Loggly depending on
+ * environment and options. If no `opts` is given or it it empty, logger 
+ * will be configured for console logging of `debug` level messages.
+ *
+ * @param {Object} [opts={}] Options hash.
+ * @param {string} opts.env Running app environment. If present, one of 
+ *   "development", "qa", or "production." If there is no environment set, 
+ *   logging will be to console irrespective of other setting. If the 
+ *   environment is "production" only Loggly will be configured.
+ * @param {boolean} opts.remote If true, log "qa" and "development"
+ *    environments to Loggly.
+ * @param {string} opts.appTag (Loggly only) Main tag for Loggly messages. If 
+ *   the environment is "qa" or "development", the environment is appended to
+ *   the tag with a hyphen (e.g. "MyApp-qa"). Additional tags may be specified
+ *   via the `tags` parameter.
+ * @param {string} opts.token (Loggly only) Token required for connecting to 
+ *   Loggly
+ * @param {string} opts.subdomain (Loggly only) Subdomain required for 
+ *   connecting to Loggly.
+ * @return {Object} A Winston logger object.
+ */
 function getLogger(opts = {}) {
   return new Winston.Logger({transports: getTransports(opts)});
 }
